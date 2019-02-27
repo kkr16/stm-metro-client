ee5b1bb5cc6d4af7f8fc460cfaf42eac9fd5d210
Karim Roukoz <karim.roukoz@ericsson.com>
2019-02-27 16:30:54 -0500

    Removed tests

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 """Tests"""
 
-from datetime import datetime
-
-from stm_metro_client import stm_metro_client
-
 
 def test():
     """Test function"""
     rcode = 0
-    stm_client = stm_metro_client.StmMetroClient()
-    api_time_start = datetime.now()
-    print(stm_client.get_state())
-    api_time_end = datetime.now()
-
-    print('API request took '
-          + str((api_time_end - api_time_start).total_seconds())
-          + ' seconds to complete.')
 
     quit(rcode)
 
